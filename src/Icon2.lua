806e5d2764b17a476bd31deb4be15781fa7bc9af
Syneezy <syneez.gg@gmail.com>
2026-05-03 17:01:21 +0700

    Create Icon2.lua

---
+++ src/Icon2.lua
@@ -0,0 +1,16 @@
+-- Icon2.lua
+-- Generated from AtCov (DO NOT EDIT MANUALLY)
+
+return {
+
+    ["activity"] = { atlas = 0, x = 114, y = 2, w = 24, h = 24 },
+    ["airplay"] = { atlas = 0, x = 170, y = 2, w = 24, h = 24 },
+    ["alarm-clock"] = { atlas = 0, x = 310, y = 2, w = 24, h = 24 },
+
+    ["arrow-left"] = { atlas = 0, x = 114, y = 30, w = 24, h = 24 },
+    ["arrow-right"] = { atlas = 0, x = 226, y = 30, w = 24, h = 24 },
+    ["arrow-up"] = { atlas = 0, x = 590, y = 30, w = 24, h = 24 },
+    ["arrow-down"] = { atlas = 0, x = 2, y = 30, w = 24, h = 24 },
+
+    -- 🔥 PASTE SEMUA 1700+ ICON DI SINI
+}
